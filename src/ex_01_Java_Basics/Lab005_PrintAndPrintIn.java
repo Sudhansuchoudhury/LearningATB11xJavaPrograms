bf911ea7a592bafdecfa65dc2ab93729385a77bb
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-11 09:27:38 +0530

    11th March: I am pushing code

---
+++ src/ex_01_Java_Basics/Lab005_PrintAndPrintIn.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab005_PrintAndPrintIn {
+    public static void main(String[] args) {
+        System.out.print("Hi My Name is KUBUL");
+        System.out.print("------");
+    System.out.println("Hi My Name is Kubul");
+    }
+
+
+}
