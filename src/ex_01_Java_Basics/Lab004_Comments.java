92dd4d8396cadf7ca986bd4d882d14342f72e4af
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-11 09:21:18 +0530

    11th March: I am pushing code

---
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+        System.out.println("a+a"); //Comment Type 1
+
+        System.out.println("b+b");
+        /* Comment type 2
+         * This is used for multiple comments with points
+         * Sample pointer
+         **/
+        System.out.println("c+c");
+        //Comment type 3//
+
+
+    }
+}
