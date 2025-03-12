4546441d9c03e6df1d6455c20a3a6c7dad95712b
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-12 21:21:27 +0530

    11th March: I am pushing code

--- src/ex_01_Java_Basics/Lab005_PrintAndPrintIn.java
+++ src/ex_01_Java_Basics/Lab005_PrintAndPrintIn.java
@@ -3,9 +3,10 @@ package ex_01_Java_Basics;
 public class Lab005_PrintAndPrintIn {
     public static void main(String[] args) {
         System.out.print("Hi My Name is KUBUL");
-        System.out.print("------");
-    System.out.println("Hi My Name is Kubul");
+        System.out.println("----");
+        {
+            System.out.println("Hi My Name is Kubul");
+        }
+
     }
-
-
 }
