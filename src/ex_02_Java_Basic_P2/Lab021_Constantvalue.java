045346fcfae737ae72d99dbafde3b89be4d025fe
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-15 22:57:14 +0530

    15th March: some new updated

---
+++ src/ex_02_Java_Basic_P2/Lab021_Constantvalue.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basic_P2;
+
+public class Lab021_Constantvalue {
+    public static void main(String[] args) {
+        final int a = 10;
+        // a = 12; //    a = 12; not possible to change the value as we have used final before the variable
+
+        final float PI = 3.14f;
+//        PI=90;same for float varibale
+        System.out.println(PI);
+    }
+}
