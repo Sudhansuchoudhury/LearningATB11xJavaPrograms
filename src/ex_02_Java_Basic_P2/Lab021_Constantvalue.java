70d25ff260cdfee552d6249335b82985be2fb363
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-16 20:58:44 +0530

    15th March: some new updated

--- src/ex_02_Java_Basic_P2/Lab021_Constantvalue.java
+++ src/ex_02_Java_Basic_P2/Lab021_Constantvalue.java
@@ -6,7 +6,9 @@ public class Lab021_Constantvalue {
         // a = 12; //    a = 12; not possible to change the value as we have used final before the variable
 
         final float PI = 3.14f;
+
 //        PI=90;same for float varibale
+//        Constant is something which value we can't change
+//        If we try then will get,error as java: cannot assign a value to final variable PI
         System.out.println(PI);
-    }
-}
+    }}
