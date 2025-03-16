1e27618ad56c0a1e704ba81e62ffc5f9b8691c04
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-16 20:52:18 +0530

    15th March: some new updated

--- src/Daily_Java_Task/Task002_MathFunctions.java
+++ src/Daily_Java_Task/Task002_MathFunctions.java
@@ -14,7 +14,7 @@ public class Task002_MathFunctions {
         int y = 10;
         int z = 10;
 
-        // cube root of (x*x + y*y - absolute vale of |z|)
+        // cube root of (x*x + y*y - absolute value of |z|)
         //Absolute value of |10|= 10, |-10|=10
         double result = Math.cbrt((x*x) + (y*y) - Math.abs(z));
         System.out.println("Result: "+ result);
