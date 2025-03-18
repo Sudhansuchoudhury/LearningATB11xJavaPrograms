92a6ad16c40c43934734653c67f906bab340d716
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-19 03:00:19 +0530

    17th March: some new updated

--- src/Daily_Java_Task/Task008_Name_Age_Salary.java
+++ src/Daily_Java_Task/Task008_Name_Age_Salary.java
@@ -3,8 +3,11 @@ package Daily_Java_Task;
 public class Task008_Name_Age_Salary {
     public static void main(String[] args) {
         String Name=args[0];
-        //String Age=args[1];
-        System.out.println("Name");
+        String Age=args[1];
+        String Salary=args[2];
+        System.out.println(Name);
+        System.out.println(Age);
+        System.out.println(Salary);
 
     }
 
--- src/Daily_Java_Task/Task009_Triangle_Classifier.java
+++ src/Daily_Java_Task/Task009_Triangle_Classifier.java
@@ -3,6 +3,9 @@ package Daily_Java_Task;
 
 public class Task009_Triangle_Classifier {
     public static void main(String[] args) {
+        int t1=Integer.parseInt(args[0]);
+        int t2=Integer.parseInt(args[1]);
+        int t3=Integer.parseInt(args[2]);
 
     }
 }
