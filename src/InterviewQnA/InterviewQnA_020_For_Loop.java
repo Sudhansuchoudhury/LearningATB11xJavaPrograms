1e792bb9c389afc179d46edf1bc546e463b82c9e
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-24 00:16:05 +0530

    24th March: some new updated

---
+++ src/InterviewQnA/InterviewQnA_020_For_Loop.java
@@ -0,0 +1,10 @@
+package InterviewQnA;
+
+public class InterviewQnA_020_For_Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i > 1; i++) {
+            System.out.println(i);
+        }
+        System.out.println("End");
+    }
+}
