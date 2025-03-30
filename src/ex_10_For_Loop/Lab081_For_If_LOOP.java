f21ec352a47779f9174a0746135807cc6c8e940b
Sudhansu.C <te.sudhansu@gmail.com>
2025-03-31 01:36:09 +0530

    24th March: some new updated

---
+++ src/ex_10_For_Loop/Lab081_For_If_LOOP.java
@@ -0,0 +1,14 @@
+package ex_10_For_Loop;
+
+public class Lab081_For_If_LOOP {
+    public static void main(String[] args) {
+        for(int ishaan=0; ishaan< 18; ishaan++){ // 0 to 17, 18 times
+        if (ishaan> 15){
+            System.out.println("Will Get Car");
+        }
+        else {
+            System.out.println("No Car");
+        }
+    }
+}
+}
